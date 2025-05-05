a5107d9f982bd07cdec6542a135691b47ff83418
ofarmer27 <oliverfarmer2023@outlook.com>
2025-05-05 17:44:23 -0600

    saving

--- src/main/java/traffic/model/LightController.java
+++ src/main/java/traffic/model/LightController.java
@@ -1,6 +1,4 @@
 package src.main.java.traffic.model;
-
-import src.main.java.traffic.interfaces.Light;
 import src.main.java.traffic.interfaces.StateControlRing;
 
 public class LightController {
@@ -45,57 +43,29 @@ public class LightController {
 
     public void updateLights()
     {
-        // simulate traffic sensors
         
-        // if timer is up
         if (topTimer >= greenLightLength)
         {
-
-            // if (controlRing.stateRingOne.determinePriority() == controlRing.stateRingOne.peekNext().getDirection())
-            // {
-
-            // }
-            // else if (controlRing.stateRingOne.determinePriority() == controlRing.stateRingOne.peek().getDirection()) {
-
-            // }
-
-            // if (controlRing.stateRingOne.determinePriority() == controlRing.stateRingOne.peekNext().getDirection())
-            // {
-
-            // }
-
             controlRing.stateRingOne.peek().setColor("red");
             controlRing.stateRingTwo.peek().setColor("red");
 
             controlRing.stateRingOne.cycle();
             controlRing.stateRingTwo.cycle();
 
-            // if both at barrier
+
             if (controlRing.stateRingOne.peek().getDirection() == "BARRIER"
                     && controlRing.stateRingTwo.peek().getDirection() == "BARRIER") {
 
                 topTimer = 0;
             }
-            // if top at barrier
             else if (controlRing.stateRingOne.peek().getDirection() == "BARRIER") {
-                // cycle bottom twice
-                // controlRing.stateRingTwo.cycle();
                 controlRing.stateRingTwo.cycle();
-
-                // cycle top once
-                // controlRing.stateRingOne.cycle();
 
                 topTimer = 0;
             }
-            // if bottom at barrier
             else if (controlRing.stateRingTwo.peek().getDirection() == "BARRIER") {
-                // cycle top twice
+
                 controlRing.stateRingOne.cycle();
-                // controlRing.stateRingOne.cycle();
-
-                //cycle bottom once
-                // controlRing.stateRingTwo.cycle();
-
                 topTimer = 0;
             }
 
